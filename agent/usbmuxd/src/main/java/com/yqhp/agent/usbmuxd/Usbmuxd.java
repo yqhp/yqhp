c800277568e2e32e64f379fb146e54563437b335
jiangyitao <451988022@163.com>
2023-04-25 17:40:23 +0800

    wip

--- agent/usbmuxd/src/main/java/com/yqhp/agent/usbmuxd/Usbmuxd.java
+++ agent/usbmuxd/src/main/java/com/yqhp/agent/usbmuxd/Usbmuxd.java
@@ -3,17 +3,9 @@ package com.yqhp.agent.usbmuxd;
 import com.dd.plist.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,146 +16,74 @@ import java.util.stream.Stream;
 public class Usbmuxd {
 
     private static final String CLIENT_VERSION = "v0.0.1";
-    private static final String PROG_NAME = "yqhp-agent";
+    private static final String PROG_NAME = "yqhp-usbmuxd";
 
-    private static final AtomicInteger TAG = new AtomicInteger(1);
+    private volatile boolean devicesListening = false;
 
-    private volatile boolean listening = false;
+    public List<IDevice> listDevices() {
+        try (UsbmuxdConnection conn = new UsbmuxdConnection()) {
+            NSDictionary req = new NSDictionary();
+            req.put("MessageType", "ListDevices");
+            req.put("ClientVersionString", CLIENT_VERSION);
+            req.put("ProgName", PROG_NAME);
 
-    public List<IDevice> getDeviceList() {
-        try (UsbmuxdConnection connection = new UsbmuxdConnection()) {
-            return getDeviceList(connection);
+            NSDictionary response = (NSDictionary) conn.writeAndRead(req);
+            NSArray deviceList = (NSArray) response.get("DeviceList");
+            return Stream.of(deviceList.getArray())
+                    .map(device -> {
+                        NSObject properties = ((NSDictionary) device).get("Properties");
+                        return new IDevice((NSDictionary) properties);
+                    }).collect(Collectors.toList());
         }
-    }
-
-    private List<IDevice> getDeviceList(UsbmuxdConnection connection) {
-        NSDictionary requestData = new NSDictionary();
-        requestData.put("MessageType", "ListDevices");
-        requestData.put("ClientVersionString", CLIENT_VERSION);
-        requestData.put("ProgName", PROG_NAME);
-
-        NSDictionary response = (NSDictionary) doRequest(connection, requestData);
-        NSArray deviceList = (NSArray) response.get("DeviceList");
-        return Stream.of(deviceList.getArray())
-                .map(device -> {
-                    NSObject properties = ((NSDictionary) device).get("Properties");
-                    return new IDevice((NSDictionary) properties);
-                }).collect(Collectors.toList());
     }
 
     public synchronized void startListenDevices(IDeviceChangeListener listener) {
-        startListenDevices(listener, null);
-    }
-
-    public synchronized void startListenDevices(IDeviceChangeListener listener, Executor executor) {
-        Runnable runnable = () -> {
-            try (UsbmuxdConnection connection = new UsbmuxdConnection()) {
-                startListenDevices(connection, listener);
-            }
-        };
-
-        if (executor == null) {
-            new Thread(runnable).start();
-        } else {
-            executor.execute(runnable);
-        }
-    }
-
-    public synchronized void startListenDevices(UsbmuxdConnection connection, IDeviceChangeListener listener) {
-        if (listening) {
+        if (devicesListening) {
             throw new IllegalStateException("listening");
         }
+        devicesListening = true;
+        new Thread(() -> {
+            try (UsbmuxdConnection conn = new UsbmuxdConnection()) {
+                // 发送监听设备请求
+                NSDictionary req = new NSDictionary();
+                req.put("MessageType", "Listen");
+                req.put("ClientVersionString", CLIENT_VERSION);
+                req.put("ProgName", PROG_NAME);
+                conn.write(req);
 
-        NSDictionary requestData = new NSDictionary();
-        requestData.put("MessageType", "Listen");
-        requestData.put("ClientVersionString", CLIENT_VERSION);
-        requestData.put("ProgName", PROG_NAME);
+                // deviceId : IDevice
+                Map<Long, IDevice> connectedDevices = new HashMap<>();
 
-        doRequest(connection.getOutputStream(), requestData);
-
-        // deviceId : IDevice
-        Map<Long, IDevice> connectedDevices = new HashMap<>();
-
-        InputStream inputStream = connection.getInputStream();
-        listening = true;
-        while (listening) {
-            NSObject response = readResponse(inputStream);
-            if (response instanceof NSDictionary) {
-                NSDictionary res = (NSDictionary) response;
-                NSObject messageType = res.get("MessageType");
-                if (messageType instanceof NSString) {
-                    String msgType = messageType.toString();
-                    if (msgType.equals("Attached")) {
-                        NSDictionary properties = (NSDictionary) res.get("Properties");
-                        IDevice iDevice = new IDevice(properties);
-                        connectedDevices.put(iDevice.getDeviceId(), iDevice);
-                        listener.deviceConnected(iDevice);
-                    } else if (msgType.equals("Detached")) {
-                        Long deviceId = ((NSNumber) res.get("DeviceID")).longValue();
-                        IDevice iDevice = connectedDevices.get(deviceId);
-                        if (iDevice != null) {
-                            listener.deviceDisconnected(iDevice);
-                            connectedDevices.remove(deviceId);
+                while (devicesListening) {
+                    NSObject response = conn.read(); // 读取数据，没有数据将阻塞在此
+                    if (response instanceof NSDictionary) {
+                        NSDictionary res = (NSDictionary) response;
+                        NSObject messageType = res.get("MessageType");
+                        if (messageType instanceof NSString) {
+                            String msgType = messageType.toString();
+                            if (msgType.equals("Attached")) {
+                                NSDictionary properties = (NSDictionary) res.get("Properties");
+                                IDevice iDevice = new IDevice(properties);
+                                connectedDevices.put(iDevice.getDeviceId(), iDevice);
+                                listener.deviceConnected(iDevice);
+                            } else if (msgType.equals("Detached")) {
+                                Long deviceId = ((NSNumber) res.get("DeviceID")).longValue();
+                                IDevice iDevice = connectedDevices.get(deviceId);
+                                if (iDevice != null) {
+                                    listener.deviceDisconnected(iDevice);
+                                    connectedDevices.remove(deviceId);
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
+        }).start();
     }
 
     public synchronized void stopListenDevices() {
-        if (listening) {
-            listening = false;
-        }
-    }
-
-    private NSObject doRequest(UsbmuxdConnection connection, NSObject requestData) {
-        doRequest(connection.getOutputStream(), requestData);
-        return readResponse(connection.getInputStream());
-    }
-
-    private void doRequest(OutputStream outputStream, NSObject requestData) {
-        byte[] payload = requestData.toXMLPropertyList()
-                .getBytes(StandardCharsets.UTF_8);
-
-        int headerLen = 16;
-        int packetLen = headerLen + payload.length;
-
-        ByteBuffer packet = ByteBuffer.allocate(packetLen);
-        packet.order(ByteOrder.LITTLE_ENDIAN);
-
-        // header
-        packet.putInt(0, packetLen);
-        packet.putInt(4, 1);
-        packet.putInt(8, 8);
-        packet.putInt(12, TAG.getAndIncrement());
-
-        // payload
-        int i = headerLen;
-        for (byte b : payload) {
-            packet.put(i++, b);
-        }
-
-        try {
-            outputStream.write(packet.array());
-        } catch (IOException e) {
-            throw new UsbmuxdException(e);
-        }
-    }
-
-    private NSObject readResponse(InputStream inputStream) {
-        try {
-            byte[] header = new byte[16];
-            inputStream.read(header);
-            ByteBuffer headerBuffer = ByteBuffer.wrap(header);
-            headerBuffer.order(ByteOrder.LITTLE_ENDIAN);
-            int payloadLen = headerBuffer.getInt(0) - 16;
-
-            byte[] payload = new byte[payloadLen];
-            inputStream.read(payload);
-            return PropertyListParser.parse(payload);
-        } catch (Exception e) {
-            throw new UsbmuxdException(e);
+        if (devicesListening) {
+            devicesListening = false;
         }
     }
 }
--- agent/usbmuxd/src/main/java/com/yqhp/agent/usbmuxd/UsbmuxdConnection.java
+++ agent/usbmuxd/src/main/java/com/yqhp/agent/usbmuxd/UsbmuxdConnection.java
@@ -1,43 +1,38 @@
 package com.yqhp.agent.usbmuxd;
 
+import com.dd.plist.NSObject;
+import com.dd.plist.PropertyListParser;
 import com.yqhp.common.commons.system.OS;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.newsclub.net.unix.AFUNIXSocket;
 import org.newsclub.net.unix.AFUNIXSocketAddress;
+import org.newsclub.net.unix.AFUNIXSocketChannel;
 
-import java.io.*;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author jiangyitao
  */
 @Slf4j
-class UsbmuxdConnection implements Closeable {
+public class UsbmuxdConnection implements Closeable {
 
     private static final int WINDOWS_USBMUXD_LOCAL_PORT = 27015;
+    private static final AtomicInteger TAG = new AtomicInteger(1);
 
-    private final Socket socket;
-    @Getter
-    private final InputStream inputStream;
-    @Getter
-    private final OutputStream outputStream;
+    private final SocketChannel socketChannel;
 
-    UsbmuxdConnection() {
+    public UsbmuxdConnection() {
         try {
-            SocketAddress address;
-            if (OS.isWindows()) {
-                socket = new Socket();
-                address = new InetSocketAddress("127.0.0.1", WINDOWS_USBMUXD_LOCAL_PORT);
-            } else {
-                socket = AFUNIXSocket.newInstance();
-                address = AFUNIXSocketAddress.of(new File("/var/run/usbmuxd"));
-            }
-            socket.connect(address);
-            inputStream = socket.getInputStream();
-            outputStream = socket.getOutputStream();
+            socketChannel = OS.isWindows()
+                    ? SocketChannel.open(new InetSocketAddress("127.0.0.1", WINDOWS_USBMUXD_LOCAL_PORT))
+                    : AFUNIXSocketChannel.open(AFUNIXSocketAddress.of(new File("/var/run/usbmuxd")));
         } catch (IOException e) {
             throw new UsbmuxdException(e);
         }
@@ -45,20 +40,55 @@ class UsbmuxdConnection implements Closeable {
 
     @Override
     public void close() {
-        try {
-            outputStream.close();
-        } catch (IOException e) {
-            log.warn("close outputStream io err", e);
+        if (socketChannel != null) {
+            try {
+                socketChannel.close();
+            } catch (IOException e) {
+                log.warn("close socket channel io err", e);
+            }
         }
+    }
+
+    public NSObject writeAndRead(NSObject req) {
+        write(req);
+        return read();
+    }
+
+    public void write(NSObject req) {
+        if (req == null) return;
+        byte[] payload = req.toXMLPropertyList().getBytes(StandardCharsets.UTF_8);
+        // header + payload
+        int packetLen = 16 + payload.length;
+        ByteBuffer packet = ByteBuffer.allocate(packetLen);
+        packet.order(ByteOrder.LITTLE_ENDIAN);
+        // header
+        packet.putInt(packetLen);
+        packet.putInt(1);
+        packet.putInt(8);
+        packet.putInt(TAG.getAndIncrement());
+        // payload
+        packet.put(payload);
+        packet.flip();
         try {
-            inputStream.close();
+            socketChannel.write(packet);
         } catch (IOException e) {
-            log.warn("close inputStream io err", e);
+            throw new UsbmuxdException(e);
         }
+    }
+
+    private final ByteBuffer respHeaderBuffer = ByteBuffer.allocate(16).order(ByteOrder.LITTLE_ENDIAN);
+
+    public NSObject read() {
         try {
-            socket.close();
-        } catch (IOException e) {
-            log.warn("close socket io err", e);
+            respHeaderBuffer.clear();
+            socketChannel.read(respHeaderBuffer);
+            // header第1个字节标识响应长度，-header=payload
+            int payloadLen = respHeaderBuffer.getInt(0) - respHeaderBuffer.capacity();
+            ByteBuffer payload = ByteBuffer.allocate(payloadLen);
+            socketChannel.read(payload);
+            return PropertyListParser.parse(payload.array());
+        } catch (Exception e) {
+            throw new UsbmuxdException(e);
         }
     }
 }
