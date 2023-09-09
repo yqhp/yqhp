caeba0678e1047be3b0e76b4d40ca9cc3e5b2f34
jiangyitao <451988022@163.com>
2023-09-09 13:30:37 +0800

    Upgrade spring

--- common/web/src/main/java/com/yqhp/common/web/config/WebMvcConfig.java
+++ common/web/src/main/java/com/yqhp/common/web/config/WebMvcConfig.java
@@ -18,7 +18,6 @@ package com.yqhp.common.web.config;
 import com.yqhp.common.web.converter.EnumConverterFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -26,17 +25,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverterFactory(new EnumConverterFactory());
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 解决跨域问题
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowCredentials(true);
-    }
 }
